e41585ba0ccf116a0ba4a8e8748778725d9b48f7
Christoffer Nielsen <nielsenchristoffer93@gmail.com>
2020-03-20 10:00:54 +0100

    modifying project section and fixing the modals

--- resources/js/script.js
+++ resources/js/script.js
@@ -57,75 +57,40 @@ $(document).ready(function () {
         }
     });
 
-    // Get the modal
-    var modal = document.getElementById("modal-1");
-    var modal2 = document.getElementById("modal-2");
-    
     // Get the button that opens the modal
-    var btn = document.getElementById("modal-1-button");
-    var btn2 = document.getElementById("modal-2-button");
+    var btn = document.querySelectorAll("button.modal-button");
+
+    // All page modals
+    var modals = document.querySelectorAll('.modal');
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("btn-exit")[0];
+    var spans = document.getElementsByClassName("close");
 
-    // When the user clicks on the button, open the modal
-    btn.onclick = function () {
-        modal.style.display = "block";
-    }
-    
-    btn2.onclick = function () {
-        modal2.style.display = "block";
+    // When the user clicks the button, open the modal
+    for (var i = 0; i < btn.length; i++) {
+        btn[i].onclick = function (e) {
+            e.preventDefault();
+            modal = document.querySelector(e.target.getAttribute("href"));
+            modal.style.display = "block";
+        }
     }
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-        modal.style.display = "none";
+    for (var i = 0; i < spans.length; i++) {
+        spans[i].onclick = function () {
+            for (var index in modals) {
+                if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+            }
+        }
     }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+        if (event.target.classList.contains('modal')) {
+            for (var index in modals) {
+                if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+            }
         }
-    }
-
-    // Get the button that opens the next slide.
-    var btnNext = document.getElementById("nextSlide");
-    
-    // Get the button that opens the next slide.
-    var btnPrev = document.getElementById("prevSlide");
-    
-    var slideIndex = 1;
-    showSlides(slideIndex);
-
-    // Next/previous controls
-    btnNext.onclick = function plusSlides() {
-        showSlides(slideIndex += 1);
-    }
-    
-    // Next/previous controls
-    btnPrev.onclick = function minusSlides() {
-        showSlides(slideIndex += -1);
-    }
-
-    function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("slide");
-        var dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {
-            slideIndex = 1
-        }
-        if (n < 1) {
-            slideIndex = slides.length
-        }
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " active";
     }
 
 });
